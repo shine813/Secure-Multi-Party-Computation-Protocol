8c2d0a7d53c7982291a6831f28d04bd3d16efaf4
Zhan Shi <phe.zshi@gmail.com>
2022-05-06 10:45:41 +0800

    SMPCP 定义文件

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@Version: 0.0.1
+@Project: Secure-Multi-Party-Computation-Protocol
+@Author: Zhan Shi
+@Time  : 2022/5/5 12:17
+@File: __init__.py
+@License: MIT
+"""
 from smpcp import SecureMultiPartyComputationProtocol
 from smpcp import CloudPlatform
 from smpcp import CloudPlatformThird
