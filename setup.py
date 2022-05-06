dce1c5778778a7eefb5567c6f2a3b39aa5d3dbfd
Zhan Shi <phe.zshi@gmail.com>
2022-05-06 11:45:50 +0800

    SETUP

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@Version: 0.0.1
+@Project: Secure-Multi-Party-Computation-Protocol
+@Author: Zhan Shi
+@Time  : 2022/5/6 11:43
+@File: setup.py
+@License: MIT
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smpcp",
+    version="0.0.1",
+    author="Zhan Shi",
+    author_email="phe.zshi@gmail.com",
+    description="Secure Multi-Party Computation Protocol base on Partially Homomorphic Encryption for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shine813/Secure-Multi-Party-Computation-Protocol",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
